6604d1d797368ac1e8c9faa3cec7b088e894b4f8
Miguel Portes <miguelszportes@gmail.com>
2025-07-14 09:19:26 -0300

    .

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import streamlit as st 
 import gdown
 import tensorflow as tf
-
+import io
+from PIL import Image
+import numpy as np
+import pandas as pd
+import plotly.express as px
+@st.cache_resource
 def carrega_modelo():
     url = 'https://drive.google.com/uc?id=1GpWix8dp6FeFAs6g0etbnw_avu9Aflfp'
     gdown.down(url, 'modelo_quantizado16bits.tflite')
@@ -10,6 +15,21 @@ def carrega_modelo():
 
     return interpreter
 
+def carrega_imagem():
+    uploaded_file = st.file_uploader('Arraste e solte uma imagem, ou clique para selecionar uma', type=['png','jpg','jpeg'])
+
+    if uploaded_file is not None:
+        image_data = uploaded_file.read()
+        image = Image.open(io.BytesIO(image_data))
+        st.image(image)
+        st.success('Imagem foi carregada com sucesso!')
+
+        image = np.array(image, dtype=np.float32)
+
+        image = image/255.0
+        image = np.expand_dims(image, axis=0)
+        return image
+
 def main():
     st.set_page_config(
         page_title = "Classifica folhas de videiras!"
@@ -17,9 +37,9 @@ def main():
     st.write("# Classifica folhas de videiras!")
 
     #carrega o modelo
-
+    interpreter = carrega_modelo()
     #carrega a imagem
-
+    image = carrega_imagem()
     #classifica a imagem
 
 if __name__ == "__main__":
