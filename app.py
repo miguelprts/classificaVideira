b60aa624fad11e441a5c28327dd0f8ff73d4d4af
Miguel Portes <miguelszportes@gmail.com>
2025-07-14 08:39:41 -0300

    app

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st 
+def main():
+    st.set_page_config(
+        page_title = "Classifica folhas de videiras!"
+    )
+    st.write("# Classifica folhas de videiras!")
+
+    #carrega o modelo
+
+    #carrega a imagem
+
+    #classifica a imagem
+
+    if__name__ == "__main__":
+        main()
