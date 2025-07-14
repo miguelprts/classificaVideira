ce98471b3533671a362a43c99d54400ab2fd0bc3
Miguel Portes <miguelszportes@gmail.com>
2025-07-14 08:57:01 -0300

    func carregaModelo

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 import streamlit as st 
+import gdown
+import tensorflow as tf
+
+def carrega_modelo():
+    url = 'https://drive.google.com/uc?id=1GpWix8dp6FeFAs6g0etbnw_avu9Aflfp'
+    gdown.down(url, 'modelo_quantizado16bits.tflite')
+    interpreter = tf.lite.interpreter(model_path='modelo_quantizado16bits.tflite')
+    interpreter.allocate_tensors()
+
+    return interpreter
+
 def main():
     st.set_page_config(
         page_title = "Classifica folhas de videiras!"
